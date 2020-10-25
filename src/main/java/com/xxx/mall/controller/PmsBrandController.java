b513f9819ffdff5f2d37ee61d6be249c62dfcd34
xinl <xinlmain@gmail.com>
2020-10-25 18:32:24 +0800

    生成ums相关实体类

--- src/main/java/com/xxx/mall/controller/PmsBrandController.java
+++ src/main/java/com/xxx/mall/controller/PmsBrandController.java
@@ -17,7 +17,7 @@ import java.util.List;
  * 品牌管理Controller
  * Created by macro on 2019/4/19.
  */
-@Api(tags = "PmsBrandController", value = "商品品牌管理")
+@Api(value = "PmsBrandController", tags = "商品品牌管理")
 @Slf4j
 @RestController
 @RequestMapping("/brand")
